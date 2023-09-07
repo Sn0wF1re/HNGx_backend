2b11dd9721332bed882d8b2ea683a5d984c6e7d8
Sn0wF1re <mokayaclinton34@gmail.com>
2023-09-07 17:23:05 +0300

    Edit time format

--- stage_one/server.js
+++ stage_one/server.js
@@ -8,21 +8,22 @@ const uri = '/api';
 
 app.get(uri, (req, res) => {
   const now = new Date();
+  const utc_time = now.toISOString().slice(0, -5) + 'Z';
   const repo = 'https://github.com/Sn0wF1re/HNGx_backend.git';
   const file = 'https://github.com/Sn0wF1re/HNGx_backend/blob/main/stage_one/server.js';
   const { slack_name, track } = req.query;
 
   const payload = {
-    'slack_name': slack_name,
-    'current_day': now.toLocaleString('en-us', { weekday: 'long' }),
-    'utc_time': now.toISOString(),
-    'track': track,
-    'github_file_url': file,
-    'github_repo_url': repo,
-    'status_code': 200
+    slack_name: slack_name,
+    current_day: now.toLocaleString('en-us', { weekday: 'long' }),
+    utc_time: utc_time,
+    track: track,
+    github_file_url: file,
+    github_repo_url: repo,
+    status_code: 200
   };
   
-  res.send(payload);
+  res.json(payload);
 });
 
 const port = process.env.PORT || 3000;
